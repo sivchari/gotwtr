3f64a1264d1ef8a21f1ca81cd38fb7ecc3df38e0
mmmommm <ryota1839@gmail.com>
2021-11-01 23:00:59 +0900

    add user lookup example

---
+++ example/user/lookup/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sivchari/gotwtr"
+)
+
+func main() {
+	client := gotwtr.New("key")
+	// look up users
+	us, err := client.LookUpUsers(context.Background(), []string{"id", "id2"})
+	if err != nil {
+		panic(err)
+	}
+	for _, u := range us.Users {
+		fmt.Println(u)
+	}
+
+	// look up user by ID
+	u, err := client.LookUpUserByID(context.Background(), "id")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(*u.User)
+
+	// look up users by username
+	uns, err := client.LookUpUsersByUserNames(context.Background(), []string{"username","username2"})
+	if err != nil {
+		panic(err)
+	}
+	for _, un := range uns.Users {
+		fmt.Println(un)
+	}
+
+	// look up user by username
+	un, err := client.LookUpUserByUserName(context.Background(), "username")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(*un.User)
+}
