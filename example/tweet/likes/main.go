220ec903a2434aecf660f3c4fdf9efb1e54d9fb0
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-11-05 22:24:07 +0900

    chore(likes): update example

--- example/tweet/likes/main.go
+++ example/tweet/likes/main.go
@@ -9,15 +9,18 @@ import (
 
 func main() {
 	client := gotwtr.New("key")
-	// get users who liked the tweet that id is "id"
-	users, err := client.LikesLookUpUsers(context.Background(), "id")
+	// get users who liked the tweet that id is "tweet_id"
+	t, err := client.LikesLookUpUsers(context.Background(), "tweet_id")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(users)
+	fmt.Println("------no option------")
+	for _, user := range t.Users {
+		fmt.Printf("id: %s, name: %s\n", user.ID, user.UserName)
+	}
 
-	// get users who liked the tweet that id is "id" with option
-	users, err = client.LikesLookUpUsers(context.Background(), "id", &gotwtr.LikesLookUpByTweetOpts{
+	// get users who liked the tweet that id is "tweet_id" with option
+	t, err = client.LikesLookUpUsers(context.Background(), "tweet_id", &gotwtr.LikesLookUpByTweetOpts{
 		Expansions:  []gotwtr.Expansion{gotwtr.ExpansionPinnedTweetID},
 		UserFields:  []gotwtr.UserField{gotwtr.UserFieldCreatedAt},
 		TweetFields: []gotwtr.TweetField{gotwtr.TweetFieldCreatedAt},
@@ -25,5 +28,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(users)
+	fmt.Println("------with option------")
+	for _, user := range t.Users {
+		fmt.Printf("id: %s, name: %s, created_at: %v\n", user.ID, user.UserName, user.CreatedAt)
+	}
+	for _, tweet := range t.Includes.Tweets {
+		fmt.Printf("tweet_id: %s, created_at: %v\n", tweet.ID, tweet.CreatedAt)
+	}
 }
