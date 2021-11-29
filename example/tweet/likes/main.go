6621c6b31018c879f189148c92caac74b2b60a31
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-11-29 14:06:03 +0900

    chore(likes): update example

--- example/tweet/likes/main.go
+++ example/tweet/likes/main.go
@@ -10,17 +10,17 @@ import (
 func main() {
 	client := gotwtr.New("key")
 	// get users who liked the tweet that id is "tweet_id"
-	t, err := client.UsersLikingTweet(context.Background(), "tweet_id")
+	ultr, err := client.UsersLikingTweet(context.Background(), "tweet_id")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("------no option------")
-	for _, user := range t.Users {
+	for _, user := range ultr.Users {
 		fmt.Printf("id: %s, name: %s\n", user.ID, user.UserName)
 	}
 
 	// get users who liked the tweet that id is "tweet_id" with option
-	t, err = client.UsersLikingTweet(context.Background(), "tweet_id", &gotwtr.UsersLikingTweetOption{
+	ultr, err = client.UsersLikingTweet(context.Background(), "tweet_id", &gotwtr.UsersLikingTweetOption{
 		Expansions:  []gotwtr.Expansion{gotwtr.ExpansionPinnedTweetID},
 		UserFields:  []gotwtr.UserField{gotwtr.UserFieldCreatedAt},
 		TweetFields: []gotwtr.TweetField{gotwtr.TweetFieldCreatedAt},
@@ -29,10 +29,36 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("------with option------")
-	for _, user := range t.Users {
+	for _, user := range ultr.Users {
 		fmt.Printf("id: %s, name: %s, created_at: %v\n", user.ID, user.UserName, user.CreatedAt)
 	}
-	for _, tweet := range t.Includes.Tweets {
-		fmt.Printf("tweet_id: %s, created_at: %v\n", tweet.ID, tweet.CreatedAt)
+	if ultr.Includes != nil {
+		for _, tweet := range ultr.Includes.Tweets {
+			fmt.Printf("tweet_id: %s, created_at: %v\n", tweet.ID, tweet.CreatedAt)
+		}
+	}
+
+	// get tweets which the user liked with no potion
+	tulr, err := client.TweetsUserLiked(context.Background(), "user_id")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("------no option------")
+	for _, tweet := range tulr.Tweets {
+		fmt.Printf("id: %s, text: %s\n", tweet.ID, tweet.Text)
+	}
+
+	// get tweets which the user liked with potion
+	tulr, err = client.TweetsUserLiked(context.Background(), "user_id", &gotwtr.TweetsUserLikedOpts{
+		TweetFields: []gotwtr.TweetField{gotwtr.TweetFieldCreatedAt, gotwtr.TweetFieldSource},
+		MaxResults:  10,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("------with option------")
+	for _, tweet := range tulr.Tweets {
+		fmt.Printf("id: %s, text: %s, created_at: %s, source: %s\n", tweet.ID, tweet.Text, tweet.CreatedAt, tweet.Source)
 	}
 }
