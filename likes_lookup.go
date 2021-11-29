a12573cb4fd0981c7ea1173e7e7150eb4b33c89e
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-11-29 13:11:37 +0900

    feat(likes): impl func

--- likes_lookup.go
+++ likes_lookup.go
@@ -51,3 +51,47 @@ func usersLikingTweet(ctx context.Context, c *client, tweetID string, opt ...*Us
 
 	return &ultr, nil
 }
+
+func tweetsUserLiked(ctx context.Context, c *client, userID string, opt ...*TweetsUserLikedOpts) (*TweetsUserLikedResponse, error) {
+	if userID == "" {
+		return nil, errors.New("tweets user liked: user id parameter is required")
+	}
+	ep := fmt.Sprintf(tweetsUserLikedURL, userID)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ep, nil)
+	if err != nil {
+		return nil, fmt.Errorf("tweets user liked new request with ctx: %w", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.bearerToken))
+
+	var topt TweetsUserLikedOpts
+	switch len(opt) {
+	case 0:
+		// do nothing
+	case 1:
+		topt = *opt[0]
+	default:
+		return nil, errors.New("tweets user liked: only one option is allowed")
+	}
+	topt.addQuery(req)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("tweets user liked: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var tulr TweetsUserLikedResponse
+	if err := json.NewDecoder(resp.Body).Decode(&tulr); err != nil {
+		return nil, fmt.Errorf("tweets user liked: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return &tulr, &HTTPError{
+			APIName: "tweets user liked",
+			Status:  resp.Status,
+			URL:     req.URL.String(),
+		}
+	}
+
+	return &tulr, nil
+}
