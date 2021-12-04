0d1c499bbc1047fd9053fdd864f3eb256678b433
mmmommm <ryota1839@gmail.com>
2021-12-04 20:52:44 +0900

    fix doc comment to be more detailed

--- doc.go
+++ doc.go
@@ -1,6 +1,12 @@
 /*
-Package gotwtr provides a Client for the Twitter V2 API.
+Package gotwtr provides a Client for the Twitter v2 APIs.
 
-This library covers all Twitter API v2 method.
+▲ [CAUTION]
+This library will cover all Twitter APIs v2 in the future,
+but please note that not all Twitter APIs are currently compatible with v2,
+so you cannot use all endpoints that were available in v1.
+
+You can check the current implementation status by clicking on the link below.
+https://github.com/sivchari/gotwtr/issues/8
 */
 package gotwtr
